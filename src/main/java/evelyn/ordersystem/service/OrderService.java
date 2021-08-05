3fd3edde4c6e447635046b1a44c5675248793af9
evelyn82ny <evelyn82ny@gmail.com>
2021-08-05 23:12:03 +0900

    Create repository and service for Order

---
+++ src/main/java/evelyn/ordersystem/service/OrderService.java
@@ -0,0 +1,45 @@
+package evelyn.ordersystem.service;
+
+import evelyn.ordersystem.domain.*;
+import evelyn.ordersystem.domain.item.Item;
+import evelyn.ordersystem.repository.ItemRepository;
+import evelyn.ordersystem.repository.MemberRepository;
+import evelyn.ordersystem.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final MemberRepository memberRepository;
+    private final OrderRepository orderRepository;
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count){
+
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+        delivery.setStatus(DeliveryStatus.READY);
+
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        orderRepository.save(order);
+        return order.getId();
+    }
+
+    @Transactional
+    public void cancelOrder(Long orderId){
+
+        Order order = orderRepository.findOne(orderId);
+        order.cancel();
+    }
+}
