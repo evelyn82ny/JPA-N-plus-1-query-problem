e918cdecf803bf52ccc4a6ec4f26e958c0de87e4
evelyn82ny <evelyn82ny@gmail.com>
2021-08-05 23:12:22 +0900

    Create OrderServiceTest

---
+++ src/test/java/evelyn/ordersystem/service/OrderServiceTest.java
@@ -0,0 +1,95 @@
+package evelyn.ordersystem.service;
+
+import evelyn.ordersystem.domain.Address;
+import evelyn.ordersystem.domain.Member;
+import evelyn.ordersystem.domain.Order;
+import evelyn.ordersystem.domain.OrderStatus;
+import evelyn.ordersystem.domain.item.Book;
+import evelyn.ordersystem.domain.item.Item;
+import evelyn.ordersystem.exception.NotEnoughStockException;
+import evelyn.ordersystem.repository.OrderRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class OrderServiceTest {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Autowired OrderService orderService;
+    @Autowired OrderRepository orderRepository;
+
+    @Test
+    public void itemOrder() throws Exception{
+        Member member = createMember();
+        Item item = createBook("JPA 연습", 10000, 10);
+        int orderCount = 2;
+
+        Long orderId = orderService.order(member.getId(), item.getId(), orderCount);
+
+        Order getOrder = orderRepository.findOne(orderId);
+
+        assertEquals(OrderStatus.ORDER, getOrder.getStatus());
+        assertEquals(1, getOrder.getOrderItems().size());
+        assertEquals(10000 * 2, getOrder.getTotalPrice());
+        assertEquals(8, item.getStockQuantity());
+    }
+
+    @Test(expected = NotEnoughStockException.class)
+    public void stockCheck() throws Exception{
+
+        Member member = createMember();
+        Item item = createBook("JPA 연습", 10000, 10);
+        int orderCount = 11;
+
+        orderService.order(member.getId(), item.getId(), orderCount);
+
+        fail("재고 수량 부족 예외가 발생해야 한다.");
+    }
+
+    @Test
+    public void cancelCheck(){
+
+        Member member = createMember();
+        Item item = createBook("JPA 연습", 10000, 10);
+        int orderCount = 2;
+
+        Long orderId = orderService.order(member.getId(), item.getId(), orderCount);
+
+        orderService.cancelOrder(orderId);
+
+        Order getOrder = orderRepository.findOne(orderId);
+
+        assertEquals(OrderStatus.CANCEL, getOrder.getStatus());
+        assertEquals(10, item.getStockQuantity());
+    }
+
+    private Member createMember() {
+        Member member = new Member();
+        member.setName("회원1");
+        member.setAddress(new Address("서울", "강가", "123-123"));
+        em.persist(member);
+        return member;
+    }
+
+    private Book createBook(String name, int price, int stockQuantity){
+        Book book = new Book();
+        book.setName(name);
+        book.setStockQuantity(stockQuantity);
+        book.setPrice(price);
+        em.persist(book);
+        return book;
+    }
+}
