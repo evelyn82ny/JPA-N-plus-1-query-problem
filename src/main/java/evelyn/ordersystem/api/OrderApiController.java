ddbb74ecf779a474f31d054759439a22f662973a
evelyn82ny <evelyn82ny@gmail.com>
2021-08-06 15:57:02 +0900

    Apply OrderSimpleQueryDto

--- src/main/java/evelyn/ordersystem/api/OrderApiController.java
+++ src/main/java/evelyn/ordersystem/api/OrderApiController.java
@@ -1,52 +1,21 @@
 package evelyn.ordersystem.api;
 
-import evelyn.ordersystem.domain.Address;
-import evelyn.ordersystem.domain.Order;
-import evelyn.ordersystem.domain.OrderStatus;
-import evelyn.ordersystem.repository.OrderRepository;
-import lombok.Data;
+import evelyn.ordersystem.repository.order.simplequery.OrderSimpleQueryDto;
+import evelyn.ordersystem.repository.order.simplequery.OrderSimpleQueryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.persistence.EntityManager;
-import java.time.LocalDateTime;
 import java.util.List;
-
-import static java.util.stream.Collectors.toList;
 
 @RestController
 @RequiredArgsConstructor
 public class OrderApiController {
 
-    private final OrderRepository orderRepository;
-    private final EntityManager em;
+    private final OrderSimpleQueryRepository orderSimpleQueryRepository;
 
     @GetMapping("/api/orders")
-    public List<SimpleOrderDto> orders(){
-        List<Order> orders = orderRepository.findAllWithMemberDelivery();
-        List<SimpleOrderDto> result = orders.stream()
-                .map(o -> new SimpleOrderDto(o))
-                .collect(toList());
-
-        return result;
-    }
-
-    @Data
-    static class SimpleOrderDto{
-
-        private Long orderId;
-        private String name;
-        private LocalDateTime orderDate;
-        private OrderStatus orderStatus;
-        private Address address;
-
-        public SimpleOrderDto(Order order){
-            orderId = order.getId();
-            name = order.getMember().getName();
-            orderDate = order.getOrderDate();
-            orderStatus = order.getStatus();
-            address = order.getDelivery().getAddress();
-        }
+    public List<OrderSimpleQueryDto> orders() {
+        return orderSimpleQueryRepository.findOrderDtos();
     }
 }
