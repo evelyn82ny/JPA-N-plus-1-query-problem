c42ed8647363f52e8d0123f05dd2739ec1312c4e
evelyn82ny <evelyn82ny@gmail.com>
2021-08-11 19:30:48 +0900

    Create OrderSearch

---
+++ src/main/java/evelyn/ordersystem/repository/OrderSearch.java
@@ -0,0 +1,12 @@
+package evelyn.ordersystem.repository;
+
+import evelyn.ordersystem.domain.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class OrderSearch {
+
+    private String memberName;
+    private OrderStatus orderStatus;
+}
